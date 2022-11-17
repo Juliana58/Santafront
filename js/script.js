e6e9030d540010ba346105df55f4d5d81d91cb9c
Juliana58 <112916259+Juliana58@users.noreply.github.com>
2022-11-17 00:39:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+function datos() {
+    alert("Autora: Molina Juliana. Email: progjuliana@gmail.com");
+    }
+    function cambia()
+        {
+            document.body.style.backgroundColor= "black";
+            document.getElementById("contenido").style.backgroundColor = "black";
+            document.getElementById("tarjetas").style.backgroundColor = "black";
+            document.getElementById("oscuro").style.backgroundColor = "black";
+            document.getElementById("oscuro").style.color = "#90ffe9";
+            document.getElementById("nav-link").style.backgroundColor = "black";
+            document.getElementById("nav-link").style.color = "black";
+            document.getElementById("objeto1").style.backgroundColor = "black";
+            document.getElementById("receta").style.backgroundColor = "black";
+            document.getElementById("receta2").style.backgroundColor = "black";
+            document.getElementById("lista").style.color = "#24eed6";
+            document.getElementById("lista2").style.color = "#24eed6";
+            document.getElementById("objeto1").style.color = "white";
+            document.getElementById("section2").style.color = "aqua";
+            document.getElementById("aside").style.backgroundColor = "black";
+            document.getElementById("social").style.backgroundColor = "black";
+            document.getElementById("section-tarjeta").style.backgroundColor = "black";
+        }
